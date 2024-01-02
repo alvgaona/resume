d1f5da599eb4a7b94c71c0938e18b5c5696277ee
Alvaro <alvgaona@gmail.com>
2024-01-02 20:53:32 -0300

    feat: add minor tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        'dark-void': '#151419',
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
