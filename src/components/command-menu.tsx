afcd51c7b8cb58a531fcf4707b78a89cf53cf8ba
Alvaro <alvgaona@gmail.com>
2024-03-10 13:32:31 -0300

    feat: adjust theme on print

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -33,9 +33,9 @@ const CommandMenu = ({ links }: Props) => {
 
   return (
     <>
-      <p className="fixed bottom-0 left-0 right-0 hidden border-t border-t-muted bg-secondary p-1 text-center text-sm text-muted-foreground dark:text-white xl:block print:hidden">
+      <p className="border-t-muted bg-secondary text-muted-foreground fixed bottom-0 left-0 right-0 hidden border-t p-1 text-center text-sm xl:block dark:text-white print:hidden">
         Press{' '}
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+        <kbd className="bg-muted text-muted-foreground pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100">
           <span className="text-lg">⌘</span>K
         </kbd>{' '}
         to open the command menu
@@ -56,8 +56,10 @@ const CommandMenu = ({ links }: Props) => {
             <CommandItem
               onSelect={async () => {
                 setOpen(false);
+                document.firstElementChild?.classList.remove('dark');
                 await new Promise((r) => setTimeout(r, 500)); // needed so the state change has time to get applied
                 window.print();
+                document.firstElementChild?.classList.add('dark');
               }}
             >
               <span className="flex items-center gap-1">
