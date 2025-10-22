42d75d450e70cbed789f7bafc6a713735997c4bb
Alvaro Gaona <alvgaona@gmail.com>
2025-10-23 01:17:37 +0200

    Remove vercel adapter
    It is unnecessary to use an adapter for a static site with Astro.

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,11 @@
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
-import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://resume.alvgaona.com',
   output: 'static',
-  adapter: vercel(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
